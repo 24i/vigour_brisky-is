d7e6f2911e2be4d91cab1306d21393af1c99716d
Jim de Beer <jim@vigour.io>
2016-06-18 14:41:13 +0200

    adhere to v3, added tests

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Promise = require('bluebird')
 Promise.config({ cancellation: true })
 
 exports.define = {
-  is (val, callback, event) {
+  is (val, callback, stamp) {
     var type = typeof val
     var compare
     var promise
@@ -28,34 +28,25 @@ exports.define = {
           _this.off('remove', cancel)
         })
         // reject
-        callback = function (data, event) {
+        callback = function (data, stamp) {
           _this.off('remove', cancel)
-          resolve(this, data, event)
+          resolve(this, data, stamp)
         }
       })
     }
 
-    if (compare.call(this, parsed, void 0, event)) {
+    if (compare.call(this, parsed, void 0, stamp)) {
       if (callback) {
-        callback.call(this, parsed, event)
+        callback.call(this, parsed, stamp)
       }
     } else {
       this.on('data', is)
     }
 
     function is (data, event) {
-      // context problems in clear
-      if (compare.call(this, this.val, data, event)) {
-        // let stored
-        // if (_this._context) {
-        //   stored = _this.storeContextChain()
-        // }
-        // _this.clearContext()
+      if (compare.call(this, this.val, data, stamp)) {
         _this.off('data', is)
-        callback.call(this, data, event)
-        // if (stored) {
-        //   _this.setContextChain(stored)
-        // }
+        callback.call(this, data, stamp)
       }
     }
     return promise || this
--- test/index.js
+++ test/index.js
@@ -1,1 +1,25 @@
 'use strict'
+const test = require('tape')
+const Observable = require('vigour-observable')
+const is = require('../')
+
+test('is - callback', function (t) {
+  t.plan(2)
+  const obs = new Observable({ inject: is })
+  obs.is('james', () => {
+    t.same(obs.emitters.data.fn.keys(), [], 'removes listeners after fire')
+  })
+  obs.set('james')
+  obs.is('james', () => {
+    t.same(obs.emitters.data.fn.keys(), [], 'removes listeners after fire')
+  })
+})
+
+test('is - promise', function (t) {
+  t.plan(1)
+  const obs = new Observable({ inject: is })
+  obs.is('james').then(() => {
+    t.same(obs.emitters.data.fn.keys(), [], 'removes listeners after fire')
+  })
+  obs.set('james')
+})
