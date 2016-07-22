1c20088938aa0ac5fcc6e8634b9f8514ab1d2070
Boite Witte <boite@vigour.io>
2016-07-22 10:41:05 +0200

    added multiple is listener tests for observable & state

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 'use strict'
 const test = require('tape')
 const Observable = require('vigour-observable')
+const State = require('vigour-state')
 const is = require('../')
 const vstamp = require('vigour-stamp')
 
@@ -16,7 +17,7 @@ test('is - callback', (t) => {
   })
 })
 
-test('is - multiple lsiteners', (t) => {
+test('is - multiple listeners', (t) => {
   t.plan(1)
   const obs = new Observable({ inject: is })
   const a = () => {}
@@ -60,4 +61,36 @@ test('is - promise - cancel on obs.remove()', (t) => {
     t.same(obs.emitters.data.fn.keys(), [], 'removed listener on cancel')
     t.end()
   }, 1000)
+})
+
+test('is - multiple is listeners on observable', (t) => {
+  t.plan(1)
+  const obs = new Observable({ inject: is })
+  let cnt = 0
+  obs.is((val) => typeof val === 'string', () => {
+    ++cnt
+  })
+  obs.is((val) => typeof val === 'string', () => {
+    ++cnt
+  })
+  obs.set('working')
+  t.equal(cnt, 2, 'is cb should be fired twice')
+})
+
+test('is - multiple is listeners on state', (t) => {
+  t.plan(1)
+  const state = new State({
+    a: {}
+  })
+  let cnt = 0
+  state.get('a.b', {}).is((val) => typeof val === 'string', function () {
+    ++cnt
+  })
+  state.get('a.b', {}).is((val) => typeof val === 'string', function () {
+    ++cnt
+  })
+  state.set({a: {
+    b: 'is'
+  }})
+  t.equal(cnt, 2, 'is cb should be fired twice')
 })
